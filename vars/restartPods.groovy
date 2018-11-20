08b7d38ab0a90f7b6a830425a33e57596a6a9e32
Gl4di4torRr <gl4di4torrr@gmail.com>
2018-11-20 06:57:25 -0500

    add restart pods shared lib

---
+++ vars/restartPods.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+class RestartPodsInput implements Serializable {
+    String projectName = ""
+    String targetApp = ""
+    String clusterUrl = ""
+    String clusterToken = ""
+}
+
+def call (Map input) {
+    call(new RestartPodsInput(input))
+}
+
+def call(RestartPodsInput input) {
+
+    assert input.targetApp?.trim() : "Param targetApp should be defined."
+    assert input.projectName?.trim() : "Param projectName should be defined."
+
+    openshift.withCluster(input.clusterUrl, input.clusterToken) {
+        openshift.withProject(input.projectName) {
+            openshift.selector("dc", input.targetApp).rollout().latest()
+        }
+    }
+}
