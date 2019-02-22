8db62edb9c2c639188a79ac174e4df008b5a1f34
Derek Wasinger <derekwasinger@gmail.com>
2019-02-22 14:03:24 -0700

    added webhook creation

--- vars/sonarqubeStaticAnalysis.groovy
+++ vars/sonarqubeStaticAnalysis.groovy
@@ -2,7 +2,8 @@
 
 class SonarQubeConfigurationInput implements Serializable {
 
-    String buildServerWebHookUrl = ""
+    String buildServerWebHookName
+    String buildServerWebHookUrl
     String dependencyCheckReportDir = 'target'
     String dependencyCheckReportFiles = 'dependency-check-report.html'
     String dependencyCheckReportName = 'OWASP Dependency Check Report'
@@ -76,11 +77,25 @@ def checkForBuildServerWebHook(SonarQubeConfigurationInput input) {
 
     withSonarQubeEnv('sonar') {
 
-        String url = input.buildServerWebHookUrl
+        println 'Validating webhook with name ${input.buildServerWebHookName} and url "${input.buildServerWebHookUrl}" exists...'
+        def retVal = sh(returnStatus: true, script: 'curl -k -u "${SONAR_AUTH_TOKEN}:" http://sonarqube:9000/api/webhooks/list | grep ${input.buildServerWebHookUrl}')
+        println "Return Value is $retVal"
 
-        def retVal = sh(returnStatus: true, script: "curl -k -u \"${SONAR_AUTH_TOKEN}:\" http://sonarqube:9000/api/webhooks/list | grep jenkins")
+        // webhook was not found
+        // create the webhook - this should be more likely be part
+        // of the sonarqube configuration automation
+        if(retVal == 1) {
+            println 'No webhook found with name ${input.buildServerWebHookName}.  Attempting to create with url "${input.buildServerWebHookUrl}"'
+            sh 'curl -k -X POST -u "${SONAR_AUTH_TOKEN}:" -F "name=${input.buildServerWebHookName}" -F "url=${input.buildServerWebHookUrl}" http://sonarqube:9000/api/webhooks/create'
+        }
 
-        println "The returned value is: $retVal"
+        // Error happened when trying to find the webhook.  Not sure if it exists
+        // so stopping so the pipeline doesn't hang waiting for the quality gate.
+        if(retVal == 2) {
+            error "Could not determine if the build server webhook was configured.  stopping to avoid hanging while waiting for quality gate."
+        }
+
+        println "Build Server Webhook found.  Continuing SonarQube analysis."
 
     }
 
