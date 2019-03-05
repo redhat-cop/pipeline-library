e1e66520c951160d30318c93de3ed5f74565d36c
Derek Wasinger <derekwasinger@gmail.com>
2019-03-05 13:20:33 -0700

    removed html reporting

--- vars/sonarqubeStaticAnalysis.groovy
+++ vars/sonarqubeStaticAnalysis.groovy
@@ -5,18 +5,6 @@ class SonarQubeConfigurationInput implements Serializable {
     String pomFile = "pom.xml"
     String buildServerWebHookName
     String buildServerWebHookUrl
-    String dependencyCheckReportDir = 'target'
-    String dependencyCheckReportFiles = 'dependency-check-report.html'
-    String dependencyCheckReportName = 'OWASP Dependency Check Report'
-    boolean dependencyCheckKeepAll = true
-    boolean dependencyCheckAlwaysLinkToLastBuild = true
-    boolean dependencyCheckAllowMissing = true
-    String unitTestReportDir = 'target/site/jacoco/'
-    String unitTestReportFiles = 'index.html'
-    String unitTestReportName = 'Jacoco Unit Test Report'
-    boolean unitTestKeepAll = true
-    boolean unitTestAlwaysLinkToLastBuild = false
-    boolean unitTestAllowMissing = true
 
 }
 
@@ -26,9 +14,7 @@ def call(Map input) {
 
 def call(SonarQubeConfigurationInput input) {
 
-    def success = true
-    def errorMsg = ''
-
+    // make sure build server web hook is available
     checkForBuildServerWebHook(input)
 
     // Execute the Maven goal sonar:sonar to attempt to generate
@@ -36,44 +22,18 @@ def call(SonarQubeConfigurationInput input) {
     withSonarQubeEnv('sonar') {
         try {
             sh 'mvn install sonar:sonar -f ${POM_FILE}'
-
-            sh 'ls -la target'
-
         } catch (error) {
-            success = false
-            errorMsg = 'Error executing sonar:sonar goal:' + error.getMessage()
+            error error.getMessage()
         }
 
     }
 
-    println "Maven command executed."
-
     // Check the quality gate to make sure 
     // it is in a passing state.
     def qualitygate = waitForQualityGate()
-    if (success && qualitygate.status != "OK") {
-        success = false
-        errorMsg = "Pipeline aborted due to quality gate failure: ${qualitygate.status}"
+    if (qualitygate.status != "OK") {
+        error "Pipeline aborted due to quality gate failure: ${qualitygate.status}"
     }
-
-    println "Quality Gate checked."
-
-    // publish the dependency check html reports
-    dependencyCheckReport(input, success)
-
-    println "Dependency Check Report Generated."
-
-    // public the unit test html reports
-    unitTestReport(input)
-
-    println "Unit Test Report Generated."
-
-    // throw error if anything happened
-    if(!success) {
-        error errorMsg
-    }
-
-    println "Done."
 
 }
 
@@ -102,36 +62,5 @@ def checkForBuildServerWebHook(SonarQubeConfigurationInput input) {
         println "Build Server Webhook found.  Continuing SonarQube analysis."
 
     }
-
-}
-
-def dependencyCheckReport(SonarQubeConfigurationInput input, boolean success) {
-
-    boolean allowMissing = input.dependencyCheckKeepAll
-
-    if(success) {
-        allowMissing = false
-    }
-
-    report(input.dependencyCheckReportDir, input.dependencyCheckReportFiles, input.dependencyCheckReportName, input.dependencyCheckKeepAll, input.dependencyCheckAlwaysLinkToLastBuild, allowMissing)
-
-}
-
-def unitTestReport(SonarQubeConfigurationInput input) {
-
-    report(input.unitTestReportDir, input.unitTestReportFiles, input.unitTestReportName, input.unitTestKeepAll, input.unitTestAlwaysLinkToLastBuild, input.unitTestAllowMissing)
-
-}
-
-def report(String pReportDir, String pReportFiles, String pReportName, boolean pKeepAll, boolean pAlwaysLinkToLastBuild, boolean pAllowMissing) {
-
-    publishHTML(target: [
-            reportDir            : pReportDir,
-            reportFiles          : pReportFiles,
-            reportName           : pReportName,
-            keepAll              : pKeepAll,
-            alwaysLinkToLastBuild: pAlwaysLinkToLastBuild,
-            allowMissing         : pAllowMissing
-    ])
 
 }
