d8c7b455af6fa13eb46928951959b3298baf583a
Andy Bryson <abryson@localhost.localdomain>
2019-05-21 13:50:14 -0400

    added logic to accomodate applier filter tags

--- vars/applier.groovy
+++ vars/applier.groovy
@@ -8,6 +8,7 @@ class ApplierInput implements Serializable{
     String skipTlsVerify = false
     String rolesPath = "galaxy"
     String applierPlaybook = "galaxy/openshift-applier/playbooks/openshift-cluster-seed.yml"
+    String filterTags = ""
 }
 
 def call(Map input) {
@@ -27,10 +28,12 @@ def call(ApplierInput input) {
             env.TOKEN = sh(script:"set +x; echo ${encodedToken} | base64 --decode", returnStdout: true)
         }
 
+        def fTags = input?.filterTags ? " -e ${input.filterTags} " : ""
+
         sh """
             oc login ${input.registryUrl} --token=${env.TOKEN} --insecure-skip-tls-verify=${input.skipTlsVerify}
             ansible-galaxy install --role-file=${input.requirementsPath} --roles-path=${input.rolesPath}
-            ansible-playbook -i ${input.inventoryPath} ${input.applierPlaybook}
+            ansible-playbook ${fTags} -i ${input.inventoryPath} ${input.applierPlaybook}
         """
     }
 }
