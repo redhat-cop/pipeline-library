07087c05b71135dd02dbd852843ff3a8b3e71342
garethahealy <garethahealy@gmail.com>
2019-08-14 12:55:16 +0100

    Removed container block as not all consumers might have this container named - call should determine not pipeline

--- vars/tagAndDeploy.groovy
+++ vars/tagAndDeploy.groovy
@@ -27,26 +27,20 @@ def call(TagAndDeployInput input) {
     assert input.deployDestinationProjectName?.trim()  : "Param deployDestinationProjectName should be defined."
     assert input.deployDestinationVersionTag?.trim()      : "Param deployDestinationVersionTag should be defined."
 
-    container('jenkins-worker-image-mgmt') {
-        script {
-            echo "Tag ${input.imageNamespace}/${input.imageName}:${input.imageVersion} as ${input.imageNamespace}/${input.imageName}:${input.deployDestinationVersionTag}"
-            sh """
-                skopeo copy \
-                    --authfile /var/run/secrets/kubernetes.io/dockerconfigjson/.dockerconfigjson \
-                    --src-tls-verify=${input.tagSourceTLSVerify} \
-                    --dest-tls-verify=${input.tagDestinationTLSVerify} \
-                    docker://${input.registryFQDN}/${input.imageNamespace}/${input.imageName}:${input.imageVersion} docker://${input.registryFQDN}/${input.imageNamespace}/${input.imageName}:${input.deployDestinationVersionTag}
-            """
-        }
-       
-        script {
-            echo "Deploy to ${input.deployDestinationProjectName}"
-            rollout(
-                clusterAPI     : input.clusterAPI,
-                clusterToken   : input.clusterToken,
-                projectName    : input.deployDestinationProjectName,
-                deploymentConfigName: input.imageName
-            )
-        }
-    }
+    echo "Tag ${input.imageNamespace}/${input.imageName}:${input.imageVersion} as ${input.imageNamespace}/${input.imageName}:${input.deployDestinationVersionTag}"
+    sh """
+        skopeo copy \
+            --authfile /var/run/secrets/kubernetes.io/dockerconfigjson/.dockerconfigjson \
+            --src-tls-verify=${input.tagSourceTLSVerify} \
+            --dest-tls-verify=${input.tagDestinationTLSVerify} \
+            docker://${input.registryFQDN}/${input.imageNamespace}/${input.imageName}:${input.imageVersion} docker://${input.registryFQDN}/${input.imageNamespace}/${input.imageName}:${input.deployDestinationVersionTag}
+    """
+
+    echo "Deploy to ${input.deployDestinationProjectName}"
+    rollout(
+        clusterAPI     : input.clusterAPI,
+        clusterToken   : input.clusterToken,
+        projectName    : input.deployDestinationProjectName,
+        deploymentConfigName: input.imageName
+    )
 }
