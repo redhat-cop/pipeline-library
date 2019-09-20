3fecd244b859fb8ea4491a60f95cd6107126c8be
garethahealy <garethahealy@gmail.com>
2019-09-20 19:13:32 +0100

    Removed 3.7 fix for deployment config container image

--- vars/applyTemplate.groovy
+++ vars/applyTemplate.groovy
@@ -32,27 +32,7 @@ def call(ApplyTemplateInput input) {
 
             def models = openshift.process(fileNameArg, parameterFileArg, "--ignore-unknown-parameters")
             echo "Creating this template will instantiate ${models.size()} objects"
-            
-            // We need to find DeploymentConfig definitions inside 
-            // So iterating trough all the objects loaded from the Template
-            for ( o in models ) {
-                if (o.kind == "DeploymentConfig") {
-                    // The bug in OCP 3.7 is that when applying DC the "Image" can't be undefined
-                    // But when using automatic triggers it updates the value for this on runtime
-                    // So when applying this dynamic value gets overwriten and breaks deployments
-                    
-                    // We will check if this DeploymentConfig already pre-exists and fetch the current value of Image
-                    // And set this Image -value into the DeploymentConfig template we are applying
-                    def dcSelector = openshift.selector("deploymentconfig/${o.metadata.name}")
-                    def foundObjects = dcSelector.exists()
-                    if (foundObjects) { 
-                        echo "This DC exists, copying the image value"
-                        def dcObjs = dcSelector.objects()
-                        echo "Image now: ${dcObjs[0].spec.template.spec.containers[0].image}"
-                        o.spec.template.spec.containers[0].image = dcObjs[0].spec.template.spec.containers[0].image
-                    }
-                }
-            }
+
             def created = openshift.apply( models )
             echo "Created: ${created.names()}"
         }
