bf95ab3e8bf010d66073b130a773799e8e0d9221
kkoller <kurt.koller@delta.com>
2020-01-07 10:40:34 -0500

    remove merge debris

--- vars/verifyDeployment.groovy
+++ vars/verifyDeployment.groovy
@@ -18,7 +18,7 @@ def call(Map input) {
 }
 
 def call(ClusterInput input) {
-    warning "verifyDeployment has been deprecated.  Please call rollout with latest:false to verify a deployment"
+    echo "verifyDeployment has been deprecated.  Please call rollout with latest:false to verify a deployment"
     assert input.targetApp?.trim(): "Param targetApp should be defined."
 
     openshift.loglevel(input.loglevel)
