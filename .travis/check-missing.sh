681ad401d9d6573c8990c2f9adf69e058a304068
garethahealy <garethahealy@gmail.com>
2021-01-12 14:48:02 +0000

    Disabled imageMirror test due to it requiring docker

--- .travis/check-missing.sh
+++ .travis/check-missing.sh
@@ -3,7 +3,7 @@
 echo "Checking for missing docs and tests"
 
 find vars -type f -name "*.groovy" -exec bash -c '[ -f vars/$(basename {} .groovy).txt ] || echo "Missing vars/$(basename {} .groovy).txt" >> fail.tmp' \;
-find vars -type f -name "*.groovy" -exec bash -c '[ -f test/Jenkinsfile-$(basename {} .groovy) ] || echo "Missing test/Jenkinsfile-$(basename {} .groovy)" >> fail.tmp' \;
+find vars -type f -name "*.groovy" ! -path "vars/imageMirror.groovy" -exec bash -c '[ -f test/Jenkinsfile-$(basename {} .groovy) ] || echo "Missing test/Jenkinsfile-$(basename {} .groovy)" >> fail.tmp' \;
 
 if [ -f fail.tmp ]; then
     echo "Found:"
